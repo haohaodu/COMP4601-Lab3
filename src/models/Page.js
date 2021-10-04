1bd6defe21b3fae1999e0409e25fcc98159bccec
victorzyang <victorzyang@cmail.carleton.ca>
2021-10-04 12:50:05 -0400

    Added the data models for the pages

---
+++ src/models/Page.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const mongoose = require("mongoose");
+
+const PageSchema = mongoose.Schema({
+  url: { type: String, required: true },
+  incoming_links: { type: Array, default: [] },
+  outgoing_links: { type: Array, default: [] },
+});
+
+module.exports = mongoose.model("Pages", ProductSchema);
---
+++ src/services/pages.js
@@ -0,0 +1,20 @@
+/** @format */
+const { validationResult, body, param } = require("express-validator");
+
+const Page = require("../models/Page");
+
+const getPages = async (req, res) => {
+
+};
+
+const getOnePage = async (req, res) => {
+
+};
+
+const getPopularPages = async (req, res) => {
+
+};
+
+const getOnePopularPage = async (req, res) => {
+
+};
